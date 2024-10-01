80087ca66acd741a29c03deae13dcd213874008c
Ruben Flemming <rfmw2007@gmail.com>
2024-10-01 11:38:44 +0200

    Implemented basic input manager

---
+++ Assets/_Project/Scripts/Managers/InputManager.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    public static InputManager Main;
+    private UserInput _userInput = new();
+
+    private Vector3 _moveDirection;
+    public Vector3 MoveDirection { get { return _moveDirection; } }
+
+    private void Awake()
+    {
+        if (Main != null)
+        {
+            Destroy(gameObject);
+        }
+
+        Main = this;
+    }
+
+    private void OnEnable()
+    {
+        _userInput.Enable();
+        _userInput.Game.Movement.performed += OnMovementPerformed;
+    }
+
+    private void OnMovementPerformed(InputAction.CallbackContext value)
+    {
+        _moveDirection = value.ReadValue<Vector3>();
+    }
+}
