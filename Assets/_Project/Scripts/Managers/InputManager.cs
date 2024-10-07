877aa63c00df1623fd792ee60642d3cf92bb016c
Ruben Flemming <rfmw2007@gmail.com>
2024-10-07 20:56:56 +0200

    Fully implemented all the actions in the input manager

--- Assets/_Project/Scripts/Managers/InputManager.cs
+++ Assets/_Project/Scripts/Managers/InputManager.cs
@@ -9,6 +9,10 @@ public class InputManager : MonoBehaviour
     public static InputManager Main;
     private UserInput _userInput;
 
+    public Action OnFire;
+    public Action OnSpecialFire;
+    public Action OnPause;
+    
     private Vector3 _moveDirection;
     public Vector3 MoveDirection { get { return _moveDirection; } }
 
@@ -26,12 +30,36 @@ public class InputManager : MonoBehaviour
 
     private void OnEnable()
     {
+        EnableUserInput();
+    }
+
+    private void EnableUserInput()
+    {
         _userInput.Enable();
+        
         _userInput.Game.Movement.performed += OnMovementPerformed;
+        _userInput.Game.Shooting.performed += OnFirePerformed;
+        _userInput.Game.SpecialFire.performed += OnSpecialFirePerformed;
+        _userInput.Game.Pausing.performed += OnPausePerformed;
     }
 
     private void OnMovementPerformed(InputAction.CallbackContext value)
     {
         _moveDirection = value.ReadValue<Vector3>();
+    }
+
+    private void OnFirePerformed(InputAction.CallbackContext value)
+    {
+        OnFire?.Invoke();
+    }
+
+    private void OnSpecialFirePerformed(InputAction.CallbackContext value)
+    {
+        OnSpecialFire?.Invoke();
+    }
+
+    private void OnPausePerformed(InputAction.CallbackContext value)
+    {
+        OnPause?.Invoke();
     }
 }
