d3f42cd48e7906de3133dd2af9a7a130fd9aabfa
Ruben Flemming <rfmw2007@gmail.com>
2024-10-24 12:25:55 +0200

    added box colliders to the enemy ships

--- Assets/_Project/Scripts/Enemies/Base_Classes/DamageableEnemy.cs
+++ Assets/_Project/Scripts/Enemies/Base_Classes/DamageableEnemy.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(BoxCollider2D))]
 public abstract class DamageableEnemy : Enemy, IDamageable
 {
     [SerializeField] private int _hitPoints;
